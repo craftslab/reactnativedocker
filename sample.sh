02e9e5105def191e377d510cfa58bf1bc7d7742d
Jia <49358172+craftslab@users.noreply.github.com>
2023-02-12 12:21:47 +0800

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Init
+# Init project
 npx react-native init AwesomeProject
+
+# Download Gradle
+curl -L https://services.gradle.org/distributions/gradle-7.5.1-all.zip -o /tmp/gradle-7.5.1-all.zip
+
+# Set distributionUrl
+pushd AwesomeProject || exit
+sed -i "s/^distributionUrl=.*$/distributionUrl=\/tmp\/gradle-7.5.1-all.zip/g" android/gradle/wrapper/gradle-wrapper.properties
+popd || exit
 
 # Build debug
 # Target: android/app/build/outputs/apk/debug/app-debug.apk
